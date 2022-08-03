e1be126a74c5a2175a7d438381b9908651898fe5
Evgenii Klepilin <evgenii.klepilin@wekynect.com>
2022-08-03 14:48:05 -0500

    Added speedometer

---
+++ index.js
@@ -0,0 +1,52 @@
+const animationDuration = 800;
+
+function initializeValues(average, min, max) {
+    const averageValue = document.getElementById("averageValue");
+    const minValue = document.getElementById("minValue");
+    const maxValue = document.getElementById("maxValue");
+
+    averageValue.innerHTML = average;
+    minValue.innerHTML = min;
+    maxValue.innerHTML = max;
+}
+
+function valueToDegrees(target, min, max) {
+    return (target - min) * 180 / (max - min);
+}
+
+function startSpeedometer() {
+    const needle = document.getElementById("needle");
+    needle.style.transform = 'rotate(180deg)';
+    setTimeout(() => needle.style.transform = 'rotate(0deg)', animationDuration + 200);
+}
+
+function moveNeedle(angle) {
+  const needle = document.getElementById("needle");
+  needle.style.transform = `rotate(${angle}deg)`;
+}
+
+function resetNeedle() {
+  const needle = document.getElementById("needle");
+  needle.style.transform = null;
+}
+
+function getEstimate() {
+    console.log("getEstimate");
+    const givenRate = parseInt(document.getElementById("givenRate").value);
+    const expectedRate = parseInt(document.getElementById("expectedRate").value);
+    const standardDeviation = parseInt(document.getElementById("standardDeviation").value);
+
+    const min = expectedRate - standardDeviation; 
+    const max = expectedRate + standardDeviation;
+    const targetDegrees = valueToDegrees(givenRate, min, max);
+
+    initializeValues(expectedRate, min, max);
+    moveNeedle(targetDegrees);
+}
+
+// function setupSlider() {
+//     const slider = document.getElementById("slider");
+//     slider.oninput = function () {
+//         moveNeedle(this.value);
+//     }
+// }
